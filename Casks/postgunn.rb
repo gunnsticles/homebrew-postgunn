de1fa41191686509de3d697950d39459e48d79ff
Gunnsticles <gunnsticles@gmail.com>
2026-09-15 14:15:24 -0700

    Casks update

--- Casks/postgunn.rb
+++ Casks/postgunn.rb
@@ -3,17 +3,21 @@ cask "postgunn" do
   
     on_arm do
       sha256 "54a98626127ae7c689241e6be79c123bda55a5dd2ad5c213ff002c342479e92a"
-      url "https://github.com/gunnsticles/postgunn/releases/download/v#{version}/Postgunn-#{version}-arm64.dmg"
+      url "https://github.com/gunnsticles/homebrew-postgunn/releases/download/v#{version}/Postgunn-#{version}-arm64.dmg"
     end
   
     on_intel do
       sha256 "27823a8846b8df955e42fd9368e09ca5fb59c2afb2855e0f97c43ec300727421"
-      url "https://github.com/gunnsticles/postgunn/releases/download/v#{version}/Postgunn-#{version}.dmg"
+      url "https://github.com/gunnsticles/homebrew-postgunn/releases/download/v#{version}/Postgunn-#{version}.dmg"
     end
   
     name "Postgunn"
-    desc "Your app description"
+    desc "Free and minimal desktop API client focused purely on the core request/response workflow. Built for developers who want to hit endpoints, organize collections locally, and test APIs without the account requirements, cloud sync, or extra bloat."
     homepage "https://github.com/gunnsticles/postgunn"
   
     app "Postgunn.app"
+
+    postflight_steps do
+      run "/usr/bin/xattr", args: ["-cr", "{{appdir}}/Postgunn.app"]
+    end
   end
